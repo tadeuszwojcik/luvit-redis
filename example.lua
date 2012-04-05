5548649f01952700771af6234537098a79d947d9
Tadeusz Wojcik <tadeuszwojcik@gmail.com>
2012-04-05 02:05:16 +0200

    get example

--- example.lua
+++ example.lua
@@ -1,7 +1,11 @@
 -- Load our native module
-local hiredis = require('./')
+local redis = require('./')
 
-local client = assert(hiredis.createClient("127.0.0.1", 6379))
+local client = assert(redis.createClient("127.0.0.1", 6379))
 client:command("SET","A","B",function(response)
+  print(response)
+end)
+
+client:command("GET","A",function(response)
   print(response)
 end)
