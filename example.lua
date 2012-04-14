627340ffdfb15649802ca356961296a8c97d4adf
Tadeusz Wojcik <tadeuszwojcik@gmail.com>
2012-04-14 18:03:57 +0200

    updated example.lua

--- example.lua
+++ example.lua
@@ -2,36 +2,38 @@ local redis = require('redis')
 
 local client = redis:new()
 
-
-client:sadd('hk1','hk2','hk3','abc','123',function(err,res)
-  if err then
-    print("error: "..err.message)
-  else
-    print("response: "..res)
-  end
-
-client:on("connect",function()
+client:on("connect", function()
   print("conect callback")
 end)
+
 client:on("disconnect",function()
   print("disconnect callback")
 end)
 
-  client:on("error",function()
-
-  print("dupaa22222 errror")
+client:on("error",function()
+  print("error callback")
 end)
 
+client:set('A','luvit rocks')
 
 local http = require("http")
 
 http.createServer(function (req, res)
-  local body = "Hello world\n"
-  res:writeHead(200, {
-    ["Content-Type"] = "text/plain",
-    ["Content-Length"] = #body
-  })
+  client:get('A', function(err, response)
+    local body = nil
+    if err then
+      body = "error: ".. err.message
+    else
+      body = "response: ".. response
+    end
+    res:writeHead(200,
+      {
+      ["Content-Type"] = "text/plain",
+      ["Content-Length"] = #body
+      })
+
   res:finish(body)
+  end)
 end):listen(8080)
 
 print("Server listening at http://localhost:8080/")
